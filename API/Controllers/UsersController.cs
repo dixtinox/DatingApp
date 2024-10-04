6c53b7aba3812df9e29a8dd9d4452f39e7d25072
dixtinox <102125841+dixtinox@users.noreply.github.com>
2024-10-04 10:38:56 -0600

    Add Users Controller

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http.HttpResults;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _context;
+
+    public UsersController(DataContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet] 
+    public ActionResult<IEnumerable<AppUser>> GetUsers()
+    {
+        var users = _context.Users.ToList();
+
+        return users;
+    }
+
+    [HttpGet("{id}")] 
+    public ActionResult<AppUser> GetUsersByID(int id)
+    {
+        var user = _context.Users.Find(id);
+
+        if (user == null) return NotFound();
+
+        return user;
+    }
+}

ee94e3f4a547588be99515c775ab498f9a78929b
dixtinox <102125841+dixtinox@users.noreply.github.com>
2024-10-06 14:06:00 -0600

    Adding DTOs

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,23 +1,25 @@
 using System.Security.Cryptography;
 using System.Text;
 using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers;
 
 public class AccountController(DataContext context) : BaseApiController
 {
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> RegisterAsync(
-        string username, string password)
+    public async Task<ActionResult<AppUser>> RegisterAsync(RegisterRequest request)
     {
-        using var hmac = new HMACSHA512();
+        if(await UserExistsAsync(request.Username)) return BadRequest("Username already in use");
 
+        using var hmac = new HMACSHA512();
         var user = new AppUser
         {
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = request.Username,
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(request.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -26,4 +28,7 @@ public class AccountController(DataContext context) : BaseApiController
         
         return user;
     }
+
+    public async Task<bool> UserExistsAsync(string username) =>
+        await context.Users.AnyAsync(user => user.UserName.ToLower() == username.ToLower());
 }

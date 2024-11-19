5ac217d915cdb7f615d4ab967d93c410d6ef372b
dixtinox <adrianlopezermeno@gmail.com>
2024-11-19 17:48:22 -0600

    Add member support

--- client/src/app/errors/test-errors/test-errors.component.ts
+++ client/src/app/errors/test-errors/test-errors.component.ts
@@ -1,5 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, inject } from '@angular/core';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'app-test-errors',
@@ -9,40 +10,40 @@ import { Component, inject } from '@angular/core';
   styleUrl: './test-errors.component.css'
 })
 export class TestErrorsComponent {
-  baseurl = "https://localhost:5001/api/";
+  baseUrl = environment.apiUrl;
   private http = inject(HttpClient);
   validationErrors: string[] = [];
 
   get400Error(): void {
-    this.http.get(this.baseurl + "buggy/bad-request").subscribe({
+    this.http.get(this.baseUrl + "buggy/bad-request").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
 
   get401Error(): void {
-    this.http.get(this.baseurl + "buggy/auth").subscribe({
+    this.http.get(this.baseUrl + "buggy/auth").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
 
   get404Error(): void {
-    this.http.get(this.baseurl + "buggy/not-found").subscribe({
+    this.http.get(this.baseUrl + "buggy/not-found").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
 
   get500Error(): void {
-    this.http.get(this.baseurl + "buggy/server-error").subscribe({
+    this.http.get(this.baseUrl + "buggy/server-error").subscribe({
       next: (response) => console.log(response),
       error: (error) => console.log(error)
     })
   }
 
   get400ValidationError(): void {
-    this.http.post(this.baseurl + "account/register", {}).subscribe({
+    this.http.post(this.baseUrl + "account/register", {}).subscribe({
       next: (response) => console.log(response),
       error: (error) => {
         console.log(error);
